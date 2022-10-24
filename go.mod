c3986803d17ddc65caa220360257e7ee1765148d
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2022-10-24 06:54:09 -0700

    2

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module go.bbkane.com/github-actions-prototype
 
 go 1.19
+
+require go.bbkane.com/gocolor v0.0.4
+
+require golang.org/x/sys v0.0.0-20220204135822-1c1b9b1eba6a // indirect
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"go.bbkane.com/gocolor"
+)
 
 func main() {
-	fmt.Println("hi")
+	color, err := gocolor.Prepare(true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(
+		color.Add(color.FgRed, "FgRed"),
+	)
 }
