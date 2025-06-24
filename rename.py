975dbe8b6e736ee622e2049865dbdf33481a06cb
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2025-06-24 04:14:10 -0400

    tweak logs

--- rename.py
+++ rename.py
@@ -47,7 +47,14 @@ def build_parser() -> argparse.ArgumentParser:
 
 def run(*args: str):
     logger.info(f"Running command: {shlex.join(args)}")
-    subprocess.run(args, check=True)
+    res = subprocess.run(
+        args,
+        check=True,
+        encoding="utf-8",
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+    logger.debug(f"stdout:\nr{res.stdout}")
 
 
 def main():
@@ -63,35 +70,35 @@ def main():
     projects_dir = pathlib.Path(__file__).parent.parent.resolve()
     src_dir = projects_dir / "example-go-cli"
     dest_dir = projects_dir / args.name
+    logger.info("Copying: %s to %s", src_dir, dest_dir)
     shutil.copytree(src_dir, dest_dir)
-    logger.info("Copied %s to %s", src_dir, dest_dir)
 
     os.chdir(dest_dir)
-    logger.info("Changed working directory to: %s", os.getcwd())
+    logger.debug("Changed working directory to: %s", os.getcwd())
 
     # git clean
     logger.info("Cleaning git repository in: %s", dest_dir)
     run("git", "clean", "-fdx")
-    logger.info("Git clean completed in: %s", dest_dir)
 
     # remove the .git directory
     git_dir = dest_dir / ".git"
+    logger.info("Removing .git directory: %s", git_dir)
     shutil.rmtree(git_dir, ignore_errors=True)
-    logger.info("Removed .git directory: %s", git_dir)
 
     # remove the rename script
     rename_script = dest_dir / "rename.py"
+    logger.info("Removing rename script: %s", rename_script)
     rename_script.unlink()
-    logger.info("Removed rename script: %s", rename_script)
 
     # replace 'example-go-cli' with the new name in all files
+    logger.info("Replacing 'example-go-cli' with '%s' in all files", args.name)
     for root, dirs, files in dest_dir.walk():
         for file in files:
             file_path: pathlib.Path = pathlib.Path(root) / file
+            logger.debug("Checking: %s", file_path)
             if file_path.suffix in (".gif",):
                 logger.debug("Skipping binary file: %s", file_path)
                 continue
-            logger.debug("Checking: %s", file_path)
             text = file_path.read_text(encoding="utf-8")
             new_text = text.replace("example-go-cli", args.name)
             if new_text != text:
@@ -99,13 +106,12 @@ def main():
                     "Replacing 'example-go-cli' with '%s' in: %s", args.name, file_path,
                 )
                 file_path.write_text(new_text, encoding="utf-8")
-    logger.info("Replaced 'example-go-cli' with '%s' in all files", args.name)
 
     # create a new git repository and commit
     logger.info("Initializing new git repository in: %s", dest_dir)
     run("git", "init")
     run("git", "add", ".")
-    run("git", "commit", "-m", f"Initial commit for {args.name}")
+    run("git", "commit", "-m", shlex.quote(f"Initial commit for {args.name}"))
     logger.info("First commit in: %s", dest_dir)
 
     logger.info("Continue steps at: https://www.bbkane.com/blog/go-project-notes/#steps")  # noqa: E501
