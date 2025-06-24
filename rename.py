c44c13b402c04e3c6b920153c0b476802c29e54a
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2025-06-24 04:53:03 -0400

    Add ColorLevelFormmatter

--- rename.py
+++ rename.py
@@ -24,6 +24,39 @@ Code at <repo>
 logger = logging.getLogger(__name__)
 
 
+class Color:
+    reset = '\x1b[0m'
+    grey = '\x1b[38;21m'
+    blue = '\x1b[38;5;39m'
+    yellow = '\x1b[38;5;226m'
+    red = '\x1b[38;5;196m'
+    bold_red = '\x1b[31;1m'
+
+
+# logic from https://stackoverflow.com/a/75339761
+class ColorLevelFormatter(logging.Formatter):
+
+    _color_levelname = {
+        'DEBUG': f"{Color.grey}DEBUG{Color.reset}",
+        'INFO': f"{Color.blue}INFO{Color.reset}",
+        'WARNING': f"{Color.yellow}WARNING{Color.reset}",
+        'ERROR': f"{Color.red}ERROR{Color.reset}",
+        'CRITICAL': f"{Color.bold_red}CRITICAL{Color.reset}",
+    }
+
+    def __init__(
+            self,
+            fmt: str = "%(levelname)s %(filename)s:%(lineno)s : %(message)s",
+            *args,
+            **kwargs,
+    ):
+        super().__init__(fmt, *args, **kwargs)
+
+    def format(self, record):
+        record.levelname = self._color_levelname[record.levelname]
+        return super().format(record)
+
+
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description=__doc__,
@@ -54,17 +87,18 @@ def run(*args: str):
         stdout=subprocess.PIPE,
         text=True,
     )
-    logger.debug(f"stdout:\nr{res.stdout}")
+    logger.debug(f"stdout:\n{res.stdout}")
 
 
 def main():
     parser = build_parser()
     args = parser.parse_args()
 
-    logging.basicConfig(
-        format="# %(levelname)s %(filename)s:%(lineno)s : %(message)s",  # noqa: E501
-        level=logging.getLevelNamesMapping()[args.log_level],
-    )
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.getLevelNamesMapping()[args.log_level])
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setFormatter(ColorLevelFormatter())
+    root_logger.addHandler(stdout_handler)
 
     # copy the example-go-cli directory to the new name
     projects_dir = pathlib.Path(__file__).parent.parent.resolve()
